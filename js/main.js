2255e133e375b9776fff396d162358ea594d65a4
Rockrayhan <khayrulalamdict@gmail.com>
2021-08-22 13:43:23 +0600

    forth puss

--- js/main.js
+++ js/main.js
@@ -1,16 +1,48 @@
 
+function innerTextToNumber(id){
+    const element = document.getElementById(id).innerText;
+    const number = parseFloat(element) ;
+    return number
+}
+
+
+
+
+
+function updateTotal() {
+    const memory16 = document.getElementById('16gb-memory');
+    const storage512 = document.getElementById('storage-256');
+    const storage1tb = document.getElementById('storage-1tb');
+    const expressDelivery = document.getElementById('express-delivery') ;
+    
+
+    const bestPrice = innerTextToNumber('best-price');
+    const memoryprice = innerTextToNumber ('memory-cost');
+    const storageCost = innerTextToNumber('storage-cost');
+    const deliveryCost = innerTextToNumber('delivery-cost');
+
+    const total = bestPrice + memoryprice + storageCost + deliveryCost ;
+
+    document.getElementById('total-cost').innerText = total ;
+    
+}
+
+
 // 8 GB memory
 document.getElementById('8gb-memory').addEventListener('click', function(){
     const memoryCostInput = document.getElementById('memory-cost');
     const memorycostNumber = memoryCostInput.innerText ;
-    memoryCostInput.innerText = parseInt(0) ;
+    memoryCostInput.innerText = '0' ;
+    updateTotal()
 })
 
 // 16 GB memory
 document.getElementById('16gb-memory').addEventListener('click', function(){
     const memoryCostInput = document.getElementById('memory-cost') ;
     const memorycostNumber = memoryCostInput.innerText ;
-    memoryCostInput.innerText = parseInt(180) ;
+    memoryCostInput.innerText = '180' ;
+    updateTotal()
+    
 })
 
 
@@ -18,7 +50,8 @@ document.getElementById('16gb-memory').addEventListener('click', function(){
 document.getElementById('storage-256').addEventListener('click', function(){
     const storageCostInput = document.getElementById('storage-cost') ;
     const storageCostNumber = storageCostInput.innerText ;
-    storageCostInput.innerText = parseInt(0) ;
+    storageCostInput.innerText = '0' ;
+    updateTotal()
     
 })
 
@@ -26,14 +59,16 @@ document.getElementById('storage-256').addEventListener('click', function(){
 document.getElementById('storage-512').addEventListener('click', function(){
     const storageCostInput = document.getElementById('storage-cost') ;
     const storageCostNumber = storageCostInput.innerText ;
-    storageCostInput.innerText = parseInt(100) ;
+    storageCostInput.innerText = '100' ;
+    updateTotal()
 })
 
 // 1 TB storage 
 document.getElementById('storage-1tb').addEventListener('click', function(){
     const storageCostInput = document.getElementById('storage-cost') ;
     const storageCostNumber = storageCostInput.innerText ;
-    storageCostInput.innerText = parseInt(180) ;
+    storageCostInput.innerText = '180' ;
+    updateTotal()
 })
 
 
@@ -41,18 +76,16 @@ document.getElementById('storage-1tb').addEventListener('click', function(){
 document.getElementById('free-delivery').addEventListener('click', function(){
     const freeDeliveryText = document.getElementById('delivery-cost');
     const freeDelivery = freeDeliveryText.innerText;
-    freeDeliveryText.innerText = parseInt(0);
+    freeDeliveryText.innerText = '0';
+    updateTotal()
 })
 
 // Express Shipping
 document.getElementById('express-delivery').addEventListener('click', function(){
     const freeDeliveryText = document.getElementById('delivery-cost');
     const freeDelivery = freeDeliveryText.innerText;
-    freeDeliveryText.innerText = parseInt(20);
+    freeDeliveryText.innerText = '20';
+    updateTotal()
 })
 
 
-// changing total price 
-// const totalText = document.getElementById('total-cost');
-// totalCost = totalText.innerText;
-// totalText.innerText = 
